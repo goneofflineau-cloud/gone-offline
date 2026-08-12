08d44275c6ecdc1d697f5a2e23358058a72aa0fd
Zoe Chan <goneofflineau@gmail.com>
2026-08-12 12:31:07 +1000

    Remove travel filter toggle, add posts to OpenTable and Regional Victoria

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,10 @@
-'use client'
-
-import { useState } from 'react'
 import { travelItems } from '@/data/media'
-import { MediaType } from '@/lib/types'
 import MediaGrid from '@/components/grid/MediaGrid'
-import FilterToggle from '@/components/ui/FilterToggle'
-
-type Filter = MediaType | 'all'
 
 export default function HomePage() {
-  const [filter, setFilter] = useState<Filter>('all')
-
-  const filtered =
-    filter === 'all' ? travelItems : travelItems.filter((i) => i.mediaType === filter)
-
   return (
     <div className="pt-28 pb-24 px-2 md:px-3">
-      <div className="flex justify-end mb-8">
-        <FilterToggle value={filter} onChange={setFilter} />
-      </div>
-      <MediaGrid items={filtered} />
+      <MediaGrid items={travelItems} />
     </div>
   )
 }
